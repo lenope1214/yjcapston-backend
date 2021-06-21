846c537df4ba893d949c2f32d5ede8c873ef9c0e
LEESEONGBOK1214 <dltjdqhr1000@gmail.com>
2021-06-21 10:45:56 +0900

    iamport shop cancel 수정

--- src/main/java/com/yjwdb2021/jumanji/service/exception/orderException/OrderNotMineException.java
+++ src/main/java/com/yjwdb2021/jumanji/service/exception/orderException/OrderNotMineException.java
@@ -6,6 +6,9 @@ public class OrderNotMineException extends BasicException {
     public OrderNotMineException(){
         super("error-504", "내 주문이 아닙니다.");
     }
+    public OrderNotMineException(String orderId){
+        super("error-504", "내 주문이 아닙니다." + orderId);
+    }
     public OrderNotMineException(String code, String message) {
         super(code, message);
     }
